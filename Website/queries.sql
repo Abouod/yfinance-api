8f92c8ea8ed047f2a650664f10c52ed076ee9823
Abdulrahman Masood <abdulrahman.masoodX@sophicautomation.com>
2024-05-06 18:31:38 +0800

    updated table

--- Website/queries.sql
+++ Website/queries.sql
@@ -10,12 +10,22 @@ CREATE TABLE IF NOT EXISTS users (
 CREATE TABLE IF NOT EXISTS details (
     id SERIAL PRIMARY KEY,
     user_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
-    manager VARCHAR(100),
-    superior VARCHAR(100),
+    manager_name VARCHAR(100),
+    superior_name VARCHAR(100),
     department VARCHAR(50),
-    phoneNumber VARCHAR(20),
-    jobTitle VARCHAR(100),
-    division VARCHAR(100)
+    phone_number VARCHAR(20),
+    job_title VARCHAR(100),
+    division VARCHAR(100),
+    employee_id VARCHAR(20),
+    passport VARCHAR(30),
+    superior_id VARCHAR(10),
+    superior_email VARCHAR(40),
+    manager_id VARCHAR(40),
+    manager_email VARCHAR(40),
+    signature text,
+    bank_name VARCHAR(30),
+    bank_account VARCHAR(100),
+    address text
 );
 CREATE TABLE IF NOT EXISTS purchase_request (
     id SERIAL PRIMARY KEY,
